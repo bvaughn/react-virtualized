9fd38aeb35690752b04770ce77df15410b96a2a3
Brian Vaughn <brian.david.vaughn@gmail.com>
2015-11-26 19:19:20 -0800

    Added gh-pages hook

---
+++ scripts/deploy-gh-pages.js
@@ -0,0 +1,13 @@
+require('babel/register')
+
+var ghpages = require('gh-pages')
+var config = require('../webpack.config.prod.js')
+
+main()
+
+function main() {
+  ghpages.publish(
+    config.output.path,
+    console.error.bind(console)
+  )
+}
--- scripts/post-build.js
+++ scripts/post-build.js
@@ -2,12 +2,15 @@ var fs = require('fs-extra')
 var path = require('path')
 var replace = require('replace')
 
-fs.copySync(path.resolve(__dirname, 'index.html'), 'build/index.html')
+const source = path.resolve(__dirname, '../index.html')
+const dest = path.resolve(__dirname, '../build/index.html')
+
+fs.copySync(source, dest)
 
 replace({
     regex: /script src="[^"]+"/,
     replacement: 'script src="demo.js"',
-    paths: ['build/index.html'],
+    paths: [dest],
     recursive: false,
     silent: true,
 });
