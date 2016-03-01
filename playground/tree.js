eb94619e8642d7b812756586d6d6378849254a32
Brian Vaughn <brian.david.vaughn@gmail.com>
2016-02-29 20:47:47 -0800

    Updated tree example

--- playground/tree.js
+++ playground/tree.js
@@ -1,70 +1,67 @@
 var ROW_HEIGHT = 20
-var data = []
+var RANDOM_WORDS = ['abstrusity', 'advertisable', 'bellwood', 'benzole', 'boreum', 'brenda', 'cassiopeian', 'chansonnier', 'cleric', 'conclusional', 'conventicle', 'copalm', 'cornopion', 'crossbar', 'disputative', 'djilas', 'ebracteate', 'ephemerally', 'epidemical', 'evasive', 'eyeglasses', 'farragut', 'fenny', 'ferryman', 'fluently', 'foreigner', 'genseng', 'glaiket', 'haunch', 'histogeny', 'illocution', 'imprescriptible', 'inapproachable', 'incisory', 'intrusiveness', 'isoceraunic', 'japygid', 'juiciest', 'jump', 'kananga', 'leavening', 'legerdemain', 'licence', 'licia', 'luanda', 'malaga', 'mathewson', 'nonhumus', 'nonsailor', 'nummary', 'nyregyhza', 'onanist', 'opis', 'orphrey', 'paganising', 'pebbling', 'penchi', 'photopia', 'pinocle', 'principally', 'prosector.', 'radiosensitive', 'redbrick', 'reexposure', 'revived', 'subexternal', 'sukarnapura', 'supersphenoid', 'tabularizing', 'territorialism', 'tester', 'thalassography', 'tuberculise', 'uncranked', 'undersawyer', 'unimpartible', 'unsubdivided', 'untwining', 'unwaived', 'webfoot', 'wedeling', 'wellingborough', 'whiffet', 'whipstall', 'wot', 'yonkersite', 'zonary']
+var data = createRandomizedData()
 
-for (var i = 0; i < 10000; i++) {
-  var item = {
-    children: [],
-    expanded: false,
-    name: 'Root ' + i
+function renderItem (item, keyPrefix) {
+  var onClick = function(event) {
+    event.stopPropagation()
+    item.expanded = !item.expanded
+    VirtualScroll.recomputeRowHeights()
   }
-  data.push(item)
-  for (var j = 0; j < i % 4; j++) {
-    item.children.push({
-      name: 'Child ' + j
-    })
+
+  var props = { key: keyPrefix }
+  var children = []
+  var itemText
+
+  if (item.expanded) {
+    props.onClick = onClick
+    itemText = '[-] ' + item.name
+    children = item.children.map(function(child, index) {
+      return renderItem(child, keyPrefix + '-' + index)
+    }) 
+  } else if (item.children.length) {
+    props.onClick = onClick
+    itemText = '[+] ' + item.name
+  } else {
+    itemText = '    '  + item.name
   }
+
+  children.unshift(
+    React.DOM.div({
+      className: 'item',
+      key: 'label',
+      style: {
+        cursor: item.children.length ? 'pointer' : 'auto'
+      }
+    }, itemText)
+  )
+
+  return React.DOM.ul(null, React.DOM.li(props, children))
+}
+
+function getExpandedItemCount (item) {
+  var count = 1
+
+  if (item.expanded) {
+    count += item.children
+      .map(getExpandedItemCount)
+      .reduce(function (total, count) { return total + count }, 0)
+  }
+
+  return count
 }
 
 var VirtualScroll
-
 function setRef (ref) {
   VirtualScroll = ref
 }
 
 function renderCell (index) {
-  var item = data[index]
-  var content = []
-
-  var prefix = item.expanded
-    ? '[-]'
-    : item.children.length > 0
-      ? '[+]'
-      : ''
-
-  content.push(
-    React.DOM.div({
-      className: 'root',
-      key: 0,
-      onClick: function () {
-        if (item.children.length) {
-          item.expanded = !item.expanded
-          VirtualScroll.recomputeRowHeights()
-        }
-      },
-    }, prefix + ' ' + item.name)
-  )
-
-  if (item.expanded) {
-    item.children.forEach(
-      function (child, index) {
-        content.push(
-          React.DOM.div({
-            className: 'child',
-            key: index + 1,
-          }, child.name)
-        )
-      }
-    )
-  }
-
-  return React.DOM.div(null, content)
+  return renderItem(data[index], index)
 }
 
 function rowHeight (index) {
-  var item = data[index]
-  return item.expanded
-    ? (item.children.length + 1) * ROW_HEIGHT
-    : ROW_HEIGHT
+  return getExpandedItemCount(data[index]) * ROW_HEIGHT
 }
 
 var App = React.createClass({
@@ -94,3 +91,30 @@ ReactDOM.render(
   React.createElement(App),
   document.querySelector('#mount')
 )
+
+function createRandomizedData () {
+  var data = []
+
+  for (var i = 0; i < 10000; i++) {
+    data.push(createRandomizedItem(0))
+  }
+
+  return data
+}
+
+function createRandomizedItem (depth) {
+  var item = {}
+  item.children = []
+  item.name = RANDOM_WORDS[ Math.floor(Math.random() * RANDOM_WORDS.length) ]
+
+  var numChildren = depth < 3
+    ? Math.floor(Math.random() * 5)
+    : 0
+  for (var i = 0; i < numChildren; i++) {
+    item.children.push(createRandomizedItem(depth + 1))
+  }
+
+  item.expanded = numChildren > 0 && Math.random() < .25
+
+  return item
+}
