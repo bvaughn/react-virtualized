868046115e3663544c84d743336e61f9d42e81de
Brian Vaughn <brian.david.vaughn@gmail.com>
2016-03-20 15:40:41 -0700

    Changed coverage format to JSON

--- karma.conf.js
+++ karma.conf.js
@@ -24,6 +24,7 @@ module.exports = function (config) {
     webpack: require('./webpack.config.dev'),
     coverageReporter: {
       dir: (process.env.CIRCLE_ARTIFACTS || '.') + '/coverage',
+      file: 'coverage.json',
       type: 'json'
     }
   })
