11b0265b307774d6e1da89302025699bd36182f4
Evgueni Naverniouk <evgueni.naverniouk@cumul8.com>
2016-04-05 10:36:24 -0700

    Fix broken build

--- source/FlexTable/FlexTable.js
+++ source/FlexTable/FlexTable.js
@@ -13,6 +13,8 @@ import SortDirection from './SortDirection'
  */
 export default class FlexTable extends Component {
   static propTypes = {
+    'aria-label': PropTypes.string,
+
     /** One or more FlexColumns describing the data displayed in this row */
     children: (props, propName, componentName) => {
       const children = React.Children.toArray(props.children)
@@ -302,8 +304,8 @@ export default class FlexTable extends Component {
         className={classNames}
         style={style}
         onClick={onClick}
-        role="gridcell"
-        tabIndex="0"
+        role='gridcell'
+        tabIndex='0'
       >
         {renderedHeader}
       </div>
@@ -341,8 +343,8 @@ export default class FlexTable extends Component {
           height: this._getRowHeight(rowIndex),
           paddingRight: scrollbarWidth
         }}
-        role="row"
-        tabIndex="0"
+        role='row'
+        tabIndex='0'
       >
         {renderedRow}
       </div>
