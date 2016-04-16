929ec9a58e4a3e2c8c1c5c6e69be3a169b486715
Brian Vaughn <brian.david.vaughn@gmail.com>
2016-04-16 11:35:26 -0700

    Added docs for getOverscanIndices.

--- source/Grid/utils/getOverscanIndices.js
+++ source/Grid/utils/getOverscanIndices.js
@@ -1,4 +1,11 @@
-// @TODO
+/**
+ * Calculates the number of cells to overscan before and after a specified range.
+ * This function ensures that overscanning doesn't exceed the available cells.
+ * @param cellCount Number of rows or columns in the current axis
+ * @param overscanCellsCount Maximum number of cells to over-render in either direction
+ * @param startIndex Begin of range of visible cells
+ * @param stopIndex End of range of visible cells
+ */
 export default function getOverscanIndices ({ cellCount, overscanCellsCount, startIndex, stopIndex }) {
   return {
     overscanStartIndex: Math.max(0, startIndex - overscanCellsCount),
