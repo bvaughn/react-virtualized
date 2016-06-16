79f83cd14128c4acb8607b20f3022630c57c4f7e
Brian Vaughn <brian.david.vaughn@gmail.com>
2016-06-15 17:23:09 -0700

    Added FlexTable playground

---
+++ playground/table.js
@@ -0,0 +1,56 @@
+const NUM_COLUMNS = 20
+
+function rowGetter (params) {
+  const fauxData = {}
+
+  for (var i = 0; i < NUM_COLUMNS; i++) {
+    fauxData[i] = `column ${i}`
+  }
+
+  return fauxData
+}
+
+var App = React.createClass({
+  render: function() {
+    const flexColumns = []
+
+    for (var i = 0; i < NUM_COLUMNS; i++) {
+      flexColumns.push(
+        React.createElement(
+          ReactVirtualized.FlexColumn,
+          {
+            dataKey: i,
+            flexGrow: 1,
+            key: i,
+            width: 50
+          }
+        )
+      )
+    }
+
+    return React.createElement(
+      ReactVirtualized.AutoSizer,
+      null,
+      function (params) {
+        return React.createElement(
+          ReactVirtualized.FlexTable,
+          {
+            height: params.height,
+            overscanRowCount: 0,
+            rowGetter,
+            rowHeight: 30,
+            rowCount: 1000,
+            width: params.width
+          },
+          null,
+          flexColumns
+        )
+      }
+    )
+  }
+})
+
+ReactDOM.render(
+  React.createElement(App),
+  document.querySelector('#mount')
+)
