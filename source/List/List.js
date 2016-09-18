88434c9c0c87aad8527db9eb489f145c3cc2d60c
Brian Vaughn <brian.david.vaughn@gmail.com>
2016-09-18 13:46:52 -0700

    Fixed an inline docs typo

--- source/List/List.js
+++ source/List/List.js
@@ -62,7 +62,7 @@ export default class List extends Component {
      */
     rowHeight: PropTypes.oneOfType([PropTypes.number, PropTypes.func]).isRequired,
 
-    /** Responsbile for rendering a row given an index; ({ index: number }): node */
+    /** Responsible for rendering a row given an index; ({ index: number }): node */
     rowRenderer: PropTypes.func.isRequired,
 
     /** Number of rows in list. */
