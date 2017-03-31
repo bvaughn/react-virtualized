018978c42d4d93d91afea6caa7315a4c60534781
Brian Vaughn <bvaughn@fb.com>
2017-03-30 23:07:34 -0700

    Fixed insecure URL in demo site

--- source/CellMeasurer/CellMeasurer.DynamicHeightList.example.js
+++ source/CellMeasurer/CellMeasurer.DynamicHeightList.example.js
@@ -48,9 +48,9 @@ export default class DynamicHeightList extends PureComponent {
     const classNames = getClassName({ columnIndex: 0, rowIndex: index })
 
     const imageWidth = 300
-    const imageHeight = datum.size * 2
+    const imageHeight = datum.size * (1 + index % 3)
 
-    const source = `http://fillmurray.com/${imageWidth}/${imageHeight}`
+    const source = `https://fillmurray.com/${imageWidth}/${imageHeight}`
 
     return (
       <CellMeasurer
