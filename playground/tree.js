8a9b1043e37bffe4ffba1e0787de97ee97300eee
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-10-15 08:49:39 -0700

    Ran Prettier over playground files

--- playground/tree.js
+++ playground/tree.js
@@ -1,129 +1,212 @@
-var ROW_HEIGHT = 20
-var RANDOM_WORDS = ['abstrusity', 'advertisable', 'bellwood', 'benzole', 'boreum', 'brenda', 'cassiopeian', 'chansonnier', 'cleric', 'conclusional', 'conventicle', 'copalm', 'cornopion', 'crossbar', 'disputative', 'djilas', 'ebracteate', 'ephemerally', 'epidemical', 'evasive', 'eyeglasses', 'farragut', 'fenny', 'ferryman', 'fluently', 'foreigner', 'genseng', 'glaiket', 'haunch', 'histogeny', 'illocution', 'imprescriptible', 'inapproachable', 'incisory', 'intrusiveness', 'isoceraunic', 'japygid', 'juiciest', 'jump', 'kananga', 'leavening', 'legerdemain', 'licence', 'licia', 'luanda', 'malaga', 'mathewson', 'nonhumus', 'nonsailor', 'nummary', 'nyregyhza', 'onanist', 'opis', 'orphrey', 'paganising', 'pebbling', 'penchi', 'photopia', 'pinocle', 'principally', 'prosector.', 'radiosensitive', 'redbrick', 'reexposure', 'revived', 'subexternal', 'sukarnapura', 'supersphenoid', 'tabularizing', 'territorialism', 'tester', 'thalassography', 'tuberculise', 'uncranked', 'undersawyer', 'unimpartible', 'unsubdivided', 'untwining', 'unwaived', 'webfoot', 'wedeling', 'wellingborough', 'whiffet', 'whipstall', 'wot', 'yonkersite', 'zonary']
-var data = createRandomizedData()
+var ROW_HEIGHT = 20;
+var RANDOM_WORDS = [
+  'abstrusity',
+  'advertisable',
+  'bellwood',
+  'benzole',
+  'boreum',
+  'brenda',
+  'cassiopeian',
+  'chansonnier',
+  'cleric',
+  'conclusional',
+  'conventicle',
+  'copalm',
+  'cornopion',
+  'crossbar',
+  'disputative',
+  'djilas',
+  'ebracteate',
+  'ephemerally',
+  'epidemical',
+  'evasive',
+  'eyeglasses',
+  'farragut',
+  'fenny',
+  'ferryman',
+  'fluently',
+  'foreigner',
+  'genseng',
+  'glaiket',
+  'haunch',
+  'histogeny',
+  'illocution',
+  'imprescriptible',
+  'inapproachable',
+  'incisory',
+  'intrusiveness',
+  'isoceraunic',
+  'japygid',
+  'juiciest',
+  'jump',
+  'kananga',
+  'leavening',
+  'legerdemain',
+  'licence',
+  'licia',
+  'luanda',
+  'malaga',
+  'mathewson',
+  'nonhumus',
+  'nonsailor',
+  'nummary',
+  'nyregyhza',
+  'onanist',
+  'opis',
+  'orphrey',
+  'paganising',
+  'pebbling',
+  'penchi',
+  'photopia',
+  'pinocle',
+  'principally',
+  'prosector.',
+  'radiosensitive',
+  'redbrick',
+  'reexposure',
+  'revived',
+  'subexternal',
+  'sukarnapura',
+  'supersphenoid',
+  'tabularizing',
+  'territorialism',
+  'tester',
+  'thalassography',
+  'tuberculise',
+  'uncranked',
+  'undersawyer',
+  'unimpartible',
+  'unsubdivided',
+  'untwining',
+  'unwaived',
+  'webfoot',
+  'wedeling',
+  'wellingborough',
+  'whiffet',
+  'whipstall',
+  'wot',
+  'yonkersite',
+  'zonary',
+];
+var data = createRandomizedData();
 
-function renderItem (item, keyPrefix) {
+function renderItem(item, keyPrefix) {
   var onClick = function(event) {
-    event.stopPropagation()
-    item.expanded = !item.expanded
-    List.recomputeRowHeights()
-    List.forceUpdate()
-  }
+    event.stopPropagation();
+    item.expanded = !item.expanded;
+    List.recomputeRowHeights();
+    List.forceUpdate();
+  };
 
-  var props = { key: keyPrefix }
-  var children = []
-  var itemText
+  var props = {key: keyPrefix};
+  var children = [];
+  var itemText;
 
   if (item.expanded) {
-    props.onClick = onClick
-    itemText = '[-] ' + item.name
+    props.onClick = onClick;
+    itemText = '[-] ' + item.name;
     children = item.children.map(function(child, index) {
-      return renderItem(child, keyPrefix + '-' + index)
-    }) 
+      return renderItem(child, keyPrefix + '-' + index);
+    });
   } else if (item.children.length) {
-    props.onClick = onClick
-    itemText = '[+] ' + item.name
+    props.onClick = onClick;
+    itemText = '[+] ' + item.name;
   } else {
-    itemText = '    '  + item.name
+    itemText = '    ' + item.name;
   }
 
   children.unshift(
-    React.DOM.div({
-      className: 'item',
-      key: 'label',
-      style: {
-        cursor: item.children.length ? 'pointer' : 'auto'
-      }
-    }, itemText)
-  )
+    React.DOM.div(
+      {
+        className: 'item',
+        key: 'label',
+        style: {
+          cursor: item.children.length ? 'pointer' : 'auto',
+        },
+      },
+      itemText,
+    ),
+  );
 
-  return React.DOM.ul(null, React.DOM.li(props, children))
+  return React.DOM.ul(null, React.DOM.li(props, children));
 }
 
-function getExpandedItemCount (item) {
-  var count = 1
+function getExpandedItemCount(item) {
+  var count = 1;
 
   if (item.expanded) {
     count += item.children
       .map(getExpandedItemCount)
-      .reduce(function (total, count) { return total + count }, 0)
+      .reduce(function(total, count) {
+        return total + count;
+      }, 0);
   }
 
-  return count
+  return count;
 }
 
-var List
-function setRef (ref) {
-  List = ref
+var List;
+function setRef(ref) {
+  List = ref;
 }
 
-function cellRenderer (params) {
-  var renderedCell = renderItem(data[params.index], params.index)
+function cellRenderer(params) {
+  var renderedCell = renderItem(data[params.index], params.index);
 
   return React.DOM.ul(
     {
       key: params.key,
-      style: params.style
+      style: params.style,
     },
-    renderedCell
-  )
+    renderedCell,
+  );
 }
 
-function rowHeight (params) {
-  return getExpandedItemCount(data[params.index]) * ROW_HEIGHT
+function rowHeight(params) {
+  return getExpandedItemCount(data[params.index]) * ROW_HEIGHT;
 }
 
 var App = React.createClass({
   render: function() {
-    return React.createElement(
-      ReactVirtualized.AutoSizer,
-      null,
-      function (params) {
-        return React.createElement(
-          ReactVirtualized.List,
-          {
-            height: params.height,
-            overscanRowCount: 10,
-            ref: setRef,
-            rowHeight: rowHeight,
-            rowRenderer: cellRenderer,
-            rowCount: data.length,
-            width: params.width
-          }
-        )
-      }
-    )
-  }
-})
+    return React.createElement(ReactVirtualized.AutoSizer, null, function(
+      params,
+    ) {
+      return React.createElement(ReactVirtualized.List, {
+        height: params.height,
+        overscanRowCount: 10,
+        ref: setRef,
+        rowHeight: rowHeight,
+        rowRenderer: cellRenderer,
+        rowCount: data.length,
+        width: params.width,
+      });
+    });
+  },
+});
 
-ReactDOM.render(
-  React.createElement(App),
-  document.querySelector('#mount')
-)
+ReactDOM.render(React.createElement(App), document.querySelector('#mount'));
 
-function createRandomizedData () {
-  var data = []
+function createRandomizedData() {
+  var data = [];
 
   for (var i = 0; i < 10000; i++) {
-    data.push(createRandomizedItem(0))
+    data.push(createRandomizedItem(0));
   }
 
-  return data
+  return data;
 }
 
-function createRandomizedItem (depth) {
-  var item = {}
-  item.children = []
-  item.name = RANDOM_WORDS[ Math.floor(Math.random() * RANDOM_WORDS.length) ]
+function createRandomizedItem(depth) {
+  var item = {};
+  item.children = [];
+  item.name = RANDOM_WORDS[Math.floor(Math.random() * RANDOM_WORDS.length)];
 
-  var numChildren = depth < 3
-    ? Math.floor(Math.random() * 5)
-    : 0
+  var numChildren = depth < 3 ? Math.floor(Math.random() * 5) : 0;
   for (var i = 0; i < numChildren; i++) {
-    item.children.push(createRandomizedItem(depth + 1))
+    item.children.push(createRandomizedItem(depth + 1));
   }
 
-  item.expanded = numChildren > 0 && Math.random() < .25
+  item.expanded = numChildren > 0 && Math.random() < 0.25;
 
-  return item
+  return item;
 }
--- playground/utils.js
+++ playground/utils.js
@@ -1,21 +1,18 @@
-function getUrlParams () {
-  var search = window.location.search
+function getUrlParams() {
+  var search = window.location.search;
 
   return search.length
     ? search
-      .substr(1)
-      .split('&')
-      .reduce(
-        function(reduced, value) {
-          var matches = value.split('=')
-          reduced[matches[0]] = matches[1]
-          return reduced
-        },
-        {}
-      )
-    : {}
+        .substr(1)
+        .split('&')
+        .reduce(function(reduced, value) {
+          var matches = value.split('=');
+          reduced[matches[0]] = matches[1];
+          return reduced;
+        }, {})
+    : {};
 }
 
-function getUrlParam (key) {
-  return getUrlParams()[key]
+function getUrlParam(key) {
+  return getUrlParams()[key];
 }
