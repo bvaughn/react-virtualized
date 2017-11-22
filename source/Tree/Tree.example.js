363a3ee1250b4b25daa41415d726dea2ea0e73f2
Vlad Rindevich <rindevich.vs@gmail.com>
2017-11-22 13:53:21 +0300

    feat(Tree): add changeable properties for example

--- source/Tree/Tree.example.js
+++ source/Tree/Tree.example.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import {AutoSizer} from '../AutoSizer';
+import {
+  ContentBox,
+  ContentBoxHeader,
+  ContentBoxParagraph,
+} from '../demo/ContentBox';
 import Tree from './Tree';
+import {InputRow, LabeledInput} from '../demo/LabeledInput';
+import styles from './Tree.example.css';
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 export default class TreeExample extends React.PureComponent {
   static overscanIndicesGetter({startIndex, stopIndex}) {
@@ -10,28 +22,127 @@ export default class TreeExample extends React.PureComponent {
   }
 
   id = 0;
-  root = this.createNode();
+  allKeys;
+  root = this._createNode();
+  state = {
+    closeAll: false,
+    dynamicRowsHeights: false,
+    height: 270,
+    rowHeight: 30,
+    scrollTo: undefined,
+  };
+  tree;
 
   constructor(...args) {
     super(...args);
+    this._nodeGetter = this._nodeGetter.bind(this);
+    this.allKeys = this._prepareAllKeys();
+  }
 
-    this.nodeGetter = this.nodeGetter.bind(this);
+  componentDidUpdate(_, {
+    closeAll: prevCloseAll,
+    dynamicRowsHeight: prevDynamicRowsHeight,
+  }) {
+    const {
+      closeAll,
+      dynamicRowsHeights,
+    } = this.state;
+
+    if (closeAll !== prevCloseAll) {
+      this.tree.setNodesStates(this._createStates(!closeAll));
+    }
+
+    if (dynamicRowsHeights !== prevDynamicRowsHeight) {
+      this.tree.forceUpdate();
+    }
   }
 
   render() {
+    const {
+      closeAll,
+      dynamicRowsHeights,
+      height,
+      rowHeight,
+      scrollTo,
+    } = this.state;
+
     return (
-      <Tree
-        height={500}
-        nodeGetter={this.nodeGetter}
-        overscanIndicesGetter={this.constructor.overscanIndicesGetter}
-        overscanRowCount={0}
-        rowHeight={30}
-        width={1024}
-      />
+      <ContentBox>
+        <ContentBoxHeader
+          text="Tree"
+          sourceLink="https://github.com/bvaughn/react-virtualized/blob/master/source/Tree/Tree.example.js"
+          docsLink="https://github.com/bvaughn/react-virtualized/blob/master/docs/Tree.md"
+        />
+
+        <ContentBoxParagraph>
+          The tree below is windowed (or "virtualized") meaning that only the
+          visible rows are rendered.
+        </ContentBoxParagraph>
+
+        <ContentBoxParagraph>
+          <label className={styles.checkboxLabel}>
+            <input
+              aria-label="Close all nodes?"
+              checked={closeAll}
+              type="checkbox"
+              onChange={event => this.setState({closeAll: event.target.checked})}
+            />
+            Close all nodes?
+          </label>
+          <label className={styles.checkboxLabel}>
+            <input
+              aria-label="Use dynamic rows heights?"
+              checked={dynamicRowsHeights}
+              type="checkbox"
+              onChange={event => this.setState({dynamicRowsHeights: event.target.checked})}
+            />
+            Use dynamic rows heights?
+          </label>
+        </ContentBoxParagraph>
+
+        <InputRow>
+          <LabeledInput
+            label="Height"
+            name="height"
+            onChange={event => this.setState({height: parseInt(event.target.value, 10) || 1})}
+            value={height}
+          />
+          <LabeledInput
+            label="Scroll to"
+            name="scrollTo"
+            placeholder="Index..."
+            onChange={event => this.setState({scrollTo: parseInt(event.target.value, 10) || undefined})}
+            value={scrollTo}
+          />
+          <LabeledInput
+            label="Row height"
+            name="rowHeight"
+            onChange={event => this.setState({rowHeight: parseInt(event.target.value, 10) || 1})}
+            value={rowHeight}
+          />
+        </InputRow>
+
+        <div>
+          <AutoSizer disableHeight>
+            {({width}) => (
+              <Tree
+                height={height}
+                nodeGetter={this._nodeGetter}
+                overscanIndicesGetter={this.constructor.overscanIndicesGetter}
+                overscanRowCount={0}
+                ref={this._setRef}
+                rowHeight={rowHeight}
+                scrollToIndex={scrollTo}
+                width={width}
+              />
+            )}
+          </AutoSizer>
+        </div>
+      </ContentBox>
     );
   }
 
-  createNode(depth = 0) {
+  _createNode(depth = 0) {
     const node = {
       id: this.id,
       name: `test-${this.id}`,
@@ -45,13 +156,25 @@ export default class TreeExample extends React.PureComponent {
     }
 
     for (let i = 0; i < 5; i++) {
-      node.children.push(this.createNode(depth + 1));
+      node.children.push(this._createNode(depth + 1));
     }
 
     return node;
   }
 
-  *nodeGetter() {
+  _createStates(value) {
+    const states = {};
+
+    for (const key of this.allKeys) {
+      states[key] = value;
+    }
+
+    return states;
+  }
+
+  * _nodeGetter() {
+    const {dynamicRowsHeights} = this.state;
+
     const stack = [];
 
     stack.push({
@@ -64,8 +187,9 @@ export default class TreeExample extends React.PureComponent {
 
       const isOpened = yield {
         childrenCount: node.children.length,
+        height: dynamicRowsHeights ? getRandomInt(30, 60) : undefined,
         id: node.id,
-        isOpenedByDefault: false,
+        isOpenedByDefault: true,
         nestingLevel,
         nodeData: node.name,
       };
@@ -79,5 +203,35 @@ export default class TreeExample extends React.PureComponent {
         }
       }
     }
+  }
+
+  _prepareAllKeys() {
+    const result = [];
+    const stack = [];
+
+    stack.push({
+      nestingLevel: 0,
+      node: this.root,
+    });
+
+    while (stack.length !== 0) {
+      const {node, nestingLevel} = stack.pop();
+      result.push(node.id);
+
+      if (node.children.length !== 0) {
+        for (let i = node.children.length - 1; i >= 0; i--) {
+          stack.push({
+            nestingLevel: nestingLevel + 1,
+            node: node.children[i],
+          });
+        }
+      }
+    }
+
+    return result;
+  }
+
+  _setRef = (instance) => {
+    this.tree = instance;
   }
 }
