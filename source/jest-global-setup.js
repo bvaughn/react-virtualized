e7bbf8b854bc260a626e007df1a229af4f7aef10
Bogdan Chadkin <trysound@yandex.ru>
2017-12-28 17:44:12 +0300

    Disable sandboxing again

--- source/jest-global-setup.js
+++ source/jest-global-setup.js
@@ -11,7 +11,10 @@ const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 
 module.exports = async function() {
   console.log('Setup Puppeteer Environment.');
-  const browser = await puppeteer.launch({headless: true});
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   global.__BROWSER__ = browser;
   await makeDir(DIR);
   await writeFile(path.join(DIR, 'wsEndpoint'), browser.wsEndpoint());
