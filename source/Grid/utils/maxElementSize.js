2dcc9d43ec5b6f0196fb5f752707b04237084633
Brian Vaughn <brian.david.vaughn@gmail.com>
2018-01-13 14:24:29 -0800

    Prettier

--- source/Grid/utils/maxElementSize.js
+++ source/Grid/utils/maxElementSize.js
@@ -1,7 +1,7 @@
 // @flow
 
 const DEFAULT_MAX_ELEMENT_SIZE = 1500000;
-const CHROME_MAX_ELEMENT_SIZE = 1.67771e+07;
+const CHROME_MAX_ELEMENT_SIZE = 1.67771e7;
 
 const isChrome = () => !!window.chrome && !!window.chrome.webstore;
 
