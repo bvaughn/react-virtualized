d907986f4d80c7a924a23fbcdb36eb7849119dae
Brian Vaughn <brian.david.vaughn@gmail.com>
2018-01-13 14:23:14 -0800

    Backed out max-element size changes for all but Chrome

--- source/Grid/utils/maxElementSize.js
+++ source/Grid/utils/maxElementSize.js
@@ -1,42 +1,14 @@
 // @flow
 
 const DEFAULT_MAX_ELEMENT_SIZE = 1500000;
+const CHROME_MAX_ELEMENT_SIZE = 1.67771e+07;
 
 const isChrome = () => !!window.chrome && !!window.chrome.webstore;
 
-const isFirefox = () => typeof window.InstallTrigger !== 'undefined';
-
-const isSafari = () =>
-  /constructor/i.test(window.HTMLElement) ||
-  (p => p.toString() === '[object SafariRemoteNotification]')(
-    !window['safari'] ||
-      (typeof window.safari !== 'undefined' && window.safari.pushNotification),
-  );
-
-const isOpera = () =>
-  (!!window.opr && !!window.opr.addons) ||
-  !!window.opera ||
-  navigator.userAgent.indexOf(' OPR/') >= 0;
-
-const elementSizesPerBrowser = {
-  chrome: 33554428,
-  safari: 33554428,
-  opera: 33554428,
-  firefox: 17895696,
-};
-
 export const getMaxElementSize = (): number => {
   if (isChrome()) {
-    return elementSizesPerBrowser.chrome;
+    return CHROME_MAX_ELEMENT_SIZE;
   }
-  if (isSafari()) {
-    return elementSizesPerBrowser.safari;
-  }
-  if (isOpera()) {
-    return elementSizesPerBrowser.opera;
-  }
-  if (isFirefox()) {
-    return elementSizesPerBrowser.firefox;
-  }
+
   return DEFAULT_MAX_ELEMENT_SIZE;
 };
