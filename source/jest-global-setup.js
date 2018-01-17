911afb13998ad5667c764801bbb18bd045b424dd
Bogdan Chadkin <trysound@yandex.ru>
2018-01-17 08:39:55 +0300

    Disable sandbox

--- source/jest-global-setup.js
+++ source/jest-global-setup.js
@@ -11,7 +11,9 @@ const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 
 module.exports = async function() {
   console.log('Setup Puppeteer Environment.');
-  const browser = await puppeteer.launch({});
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   global.__BROWSER__ = browser;
   await makeDir(DIR);
   await writeFile(path.join(DIR, 'wsEndpoint'), browser.wsEndpoint());
