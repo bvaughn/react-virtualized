5fa6683a461bfc71fe1a084db6b5ea6b864b68b3
roncrush <syedali069@gmail.com>
2018-09-04 14:04:56 -0400

    Fix MultiGrid's recomputeGridSize() to force update if grid width or height change

--- source/MultiGrid/MultiGrid.js
+++ source/MultiGrid/MultiGrid.js
@@ -156,10 +156,15 @@ class MultiGrid extends React.PureComponent {
         columnIndex: adjustedColumnIndex,
         rowIndex,
       });
-
+	  
+    const oldLeftGridWidth = this._leftGridWidth;
+    const oldTopGridHeight = this._topGridHeight;
     this._leftGridWidth = null;
     this._topGridHeight = null;
     this._maybeCalculateCachedStyles(true);
+	if (oldLeftGridWidth !== this._leftGridWidth || oldTopGridHeight !== this._topGridHeight) {
+       this.forceUpdate();
+     }
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
